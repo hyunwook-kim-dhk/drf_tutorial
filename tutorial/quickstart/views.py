86ad5097563446af35168c789cd8045c493b4cde
hyunwook-kim-dhk <hyunwook.kim@deliveryhero.co.kr>
2021-12-30 10:51:16 +0900

    add serializers, viewsets, urls, paginations

--- tutorial/quickstart/views.py
+++ tutorial/quickstart/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework import permissions
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows group to be viewed or edited
+    """
+    queryset = Group.objects.all()
+    serizlier_class = GroupSerializer
+    permission_classes = [permissinos.IsAuthenticated]
